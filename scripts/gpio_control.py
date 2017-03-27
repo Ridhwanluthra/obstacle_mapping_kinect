643dab723c9bb91b16f6e291156e19a8ad5e5e6f
Pankaj-Baranwal <pankajisadeveloper@gmail.com>
2017-03-27 20:48:34 +0530

    I dont know. Feeling sleepy

--- scripts/gpio_control.py
+++ scripts/gpio_control.py
@@ -9,9 +9,7 @@
 * global variables: curr_frame, data_per_frame, check
 *
 '''
-import rospy
-from std_msgs.msg import String, Float64MultiArray, MultiArrayLayout, MultiArrayDimension
-import requests
+# import rospy
 import math
 from time import sleep
 import RPi.GPIO as gpio
@@ -69,64 +67,7 @@ def reset():
 * Example Call:  callback function, manual calling not required.
 *
 '''
-def callback(data):
-    global val, curr_frame, data_per_frame, sentence, width, minDistance, min_angle, left_most, right_most, direction
-
-    counter = data.data[0]
-    current_width = (data.data[1])
-    currentMinDistance = (data.data[2])
-    current_min_angle = (data.data[3])
-    current_left_most = (data.data[4])
-    current_right_most = (data.data[5])
-
-    if curr_frame == data.data[0]:
-        data_per_frame.append(data.data)
-    else:
-        for dat in data_per_frame:
-            width.append(round(dat[1], 2))
-            minDistance.append(round(dat[2], 2))
-            min_angle.append(round(dat[3], 2))
-            left_most.append(round(dat[4], 2))
-            right_most.append(round(dat[5], 2))
-            
-            direction.append("right" if min_angle[-1] > 0 else "left")
-        for i in range(len(width)):
-            if width > 0.0:
-                if min_angle[i]>15:
-                    switch_on(pin3)
-                elif min_angle[i]<-15:
-                    switch_on(pin1)
-                else:
-                    switch_on(pin2)
-                sleep(0.5)
-        reset()
-        width = list()
-        angle = list()
-        direction = list()
-        curr_frame = data.data[0]
-        data_per_frame = list()
-
-'''
-*
-* Function Name:    listener
-* Input:        NIL
-* Output:       NIL
-* Logic:    Initializes node and makes sure that two nodes don't have the same name, 
-*           also subscribes to the topic and calls the callback function.
-* Example Call:  listener()
-*
-'''
-def listener():
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("cluster_distances", Float64MultiArray, callback)
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
 
 # runs the listener function if the file is run as a script
 if __name__ == '__main__':
-    listener()
+    switch_on(pin3)
