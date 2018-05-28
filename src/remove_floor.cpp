df8dbad00c8f3f40cbe456e988f2e6fc3c42f12e
ridhwanluthra <ridhwancode@gmail.com>
2018-05-28 06:26:11 +0530

    functionality for removing plane working

--- src/remove_floor.cpp
+++ src/remove_floor.cpp
@@ -32,7 +32,7 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/filters/extract_indices.h>
 
-ros::Publisher pub;
+ros::Publisher voxel_pub, pub;
 /*
 *
 * Function Name: callback
@@ -44,25 +44,21 @@ ros::Publisher pub;
 * Example Call: Callback function. Manual calling not required. 
 *
 */
-void callback (const sensor_msgs::PointCloud2ConstPtr& input) {
-  //variable declarations
-  pcl::PCLPointCloud2* cloud_blob = new pcl::PCLPointCloud2; 
-  pcl::PCLPointCloud2ConstPtr cloudPtr(cloud_blob);
-  pcl::PCLPointCloud2 cloud_filtered_blob;
-  
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>), cloud_p (new pcl::PointCloud<pcl::PointXYZRGB>), cloud_f (new pcl::PointCloud<pcl::PointXYZRGB>);
-
-  pcl_conversions::toPCL(*input, *cloud_blob);
+void callback (const pcl::PCLPointCloud2ConstPtr& cloud) {
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_voxeled (new pcl::PointCloud<pcl::PointXYZRGB>);
 
   // this is the voxel grid filtering
   // Create the filtering object: downsample the dataset using a leaf size of 1cm
-  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-  sor.setInputCloud (cloudPtr);
-  sor.setLeafSize (0.01f, 0.01f, 0.01f);
-  sor.filter (cloud_filtered_blob);
+  pcl::VoxelGrid<pcl::PCLPointCloud2> voxel;
+  voxel.setInputCloud (cloud);
+  voxel.setLeafSize (0.01f, 0.01f, 0.01f);
+  pcl::PCLPointCloud2 cloud_voxeled_pcl2;
+  voxel.filter (cloud_voxeled_pcl2);
+
+  voxel_pub.publish(cloud_voxeled_pcl2);
 
   // Convert to the templated PointCloud
-  pcl::fromPCLPointCloud2 (cloud_filtered_blob, *cloud_filtered);
+  pcl::fromPCLPointCloud2 (cloud_voxeled_pcl2, *cloud_voxeled);
 
   pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
   pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
@@ -74,31 +70,33 @@ void callback (const sensor_msgs::PointCloud2ConstPtr& input) {
   seg.setMethodType (pcl::SAC_RANSAC);
   seg.setMaxIterations (1000);
   seg.setDistanceThreshold (0.01);
-  seg.setInputCloud (cloud_filtered);
+  seg.setInputCloud (cloud_voxeled);
     
   seg.segment(*inliers, *coefficients);
 
   // Exit if no plane found
-  if (inliers->indices.size () == 0) return;
+  if (inliers->indices.size() == 0) return;
 
   // Extract indices to find the point cloud without floor
   pcl::ExtractIndices<pcl::PointXYZRGB> extract;
-  extract.setInputCloud(cloud_filtered);
+  extract.setInputCloud(cloud_voxeled);
   extract.setIndices(inliers);
   // set setNegative to true to get the point cloud without floor
   // if set to false we will get a point cloud of just the floor
   extract.setNegative(true);
-  extract.filter(*cloud_f);
-  pcl::PointCloud<pcl::PointXYZRGB> cloud_xyzrgb = *cloud_f;
+  extract.filter(*cloud_voxeled);
 
-  // colors the point cloud red
-  for (size_t i = 0; i < cloud_xyzrgb.points.size(); i++) {
-    cloud_xyzrgb.points[i].r = 255;
-  }
+  
+  // pcl::PointCloud<pcl::PointXYZRGB> cloud_xyzrgb = *cloud_voxeled;
+
+  // // colors the point cloud red
+  // for (size_t i = 0; i < cloud_xyzrgb.points.size(); i++) {
+  //   cloud_xyzrgb.points[i].r = 255;
+  // }
 
   // Publish the plane removed cloud to a new topic.
   pcl::PCLPointCloud2 outcloud;
-  pcl::toPCLPointCloud2 (cloud_xyzrgb, outcloud);
+  pcl::toPCLPointCloud2 (*cloud_voxeled, outcloud);
   pub.publish (outcloud);
 }
 
@@ -114,6 +112,7 @@ main (int argc, char** argv)
 
   // Create a ROS publisher for the output point cloud
   pub = nh.advertise<sensor_msgs::PointCloud2> ("plane_removed", 1);
+  voxel_pub = nh.advertise<sensor_msgs::PointCloud2> ("voxeled", 1);
 
   // Spin
   ros::spin();
